9beab7b0880d95e9f0fe8780e7d8c137449a1edc
madhavidudhat484-code <madhavidudhat484@gmail.com>
2026-03-18 00:54:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,12 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+
+class Config:
+    DB_HOST = os.getenv("DB_HOST", "localhost")
+    DB_USER = os.getenv("DB_USER", "DatabaseUser")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "DatabasePassword")
+    DB_NAME = os.getenv("DB_NAME", "dairy_management")
+    SECRET_KEY = os.getenv("SECRET_KEY", "dairy_secret_key")
